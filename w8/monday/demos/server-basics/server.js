28dd2e3f6e3c9b797c644a331408ef4dfb2e1d54
Javier Ortiz <javier.ortiz989@gmail.com>
2022-03-07 14:20:55 -0800

    week 8 monday

---
+++ w8/monday/demos/server-basics/server.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const fs = require("fs");
+
+let database = [];
+const server = http.createServer((req, res) => {
+    console.log(`${req.method} ${req.url}`);
+        
+    if (req.method === "GET" && req.url === "/") {
+        const htmlPage = fs.readFileSync("index.html", "utf-8");
+        const tasksList = database.map(task => {
+            return `<li>${task["tasks"]} - ${task["time"]}</li>`
+        });
+        const resBody = htmlPage
+            .replace("${tasks}", tasksList.join(""));
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        return res.end(resBody);
+    }
+
+    if (req.method === "GET" && req.url === "/main.css") {
+        const resBody = fs.readFileSync("main.css");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        return res.end(resBody);
+    }
+
+    let reqBody = "";
+    req.on("data", (data) => {
+        reqBody += data;
+    });
+
+    req.on("end", () => {
+        // Parsing the body of the request
+        if (reqBody) {
+            req.body = reqBody
+                .split("&")
+                .map((keyValuePair) => keyValuePair.split("="))
+                .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+                .map(([key, value]) => [key, decodeURIComponent(value)])
+                .reduce((acc, [key, value]) => {
+                    acc[key] = value;
+                    return acc;
+                }, {});
+            console.log(req.body);
+        }
+        if (req.method === "POST" && req.url === "/tasks") {
+            console.log(req.body);
+            database.push(req.body);
+            res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
+        }
+
+        res.statusCode = 404;
+        res.end('URL not found');
+    });
+    
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listening on port', port));
