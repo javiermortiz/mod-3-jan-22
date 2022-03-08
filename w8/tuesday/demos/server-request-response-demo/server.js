d5b7e97135584fdef30d1a950b0753dd9300550b
Javier Ortiz <javier.ortiz989@gmail.com>
2022-03-08 10:28:52 -0800

    phase 2

--- w8/tuesday/demos/server-request-response-demo/server.js
+++ w8/tuesday/demos/server-request-response-demo/server.js
@@ -1,13 +1,48 @@
 const http = require('http');
 const fs = require("fs");
+const { runInNewContext } = require('vm');
 
+let dailyMessage = "hello";
 const server = http.createServer((req, res) => {
     console.log(`${req.method} ${req.url}`);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Hello World</h1>');
-    return res.end();
+    if (req.method === 'GET' && req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+
+        res.write(`${dailyMessage}`);
+        res.write(`<form action="/messages" method="post"><input name="dailyMessage"></form>`)
+        return res.end();
+    }
+
+    let reqBody = "";
+    req.on("data", (data) => {
+        reqBody += data;
+    });
+
+    req.on("end", () => {
+        // Parsing the body of the request
+        if (reqBody) {
+            req.body = reqBody
+                .split("&")
+                .map((keyValuePair) => keyValuePair.split("="))
+                .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+                .map(([key, value]) => [key, decodeURIComponent(value)])
+                .reduce((acc, [key, value]) => {
+                    acc[key] = value;
+                    return acc;
+                }, {});
+            console.log(req.body);
+        }
+
+        if (req.method === 'POST' && req.url === '/messages') {
+            dailyMessage = req.body.dailyMessage;
+            res.statusCode = 302;
+            res.setHeader('location', '/');
+            res.end();
+            return;
+        }
+    });
 });
 
 const port = 5000;
