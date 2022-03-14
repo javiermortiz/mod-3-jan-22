7c962ecee766b50d006487e1027ffae38d7827d3
Javier Ortiz <javier.ortiz989@gmail.com>
2022-03-14 13:53:29 -0700

    script

--- w9/monday/demos/browser-basics-demo/index.js
+++ w9/monday/demos/browser-basics-demo/index.js
@@ -1,16 +1,16 @@
-console.log("hello world");
-document.getElementById("playground").innerHTML = "<h2>Hello World</h2>";
+// console.log("hello world");
+// document.getElementById("playground").innerHTML = "<h2>Hello World</h2>";
 
-// import instructors from "./seedData.js";
+import instructors from "./seedData.js";
 
-// const helloInstructors = instructors.map(instructor => `<h2>Hello ${instructor}</h2>`);
-// document.getElementById("playground").innerHTML = helloInstructors.join("");
+const helloInstructors = instructors.map(instructor => `<h2>Hello ${instructor}</h2>`);
+document.getElementById("playground").innerHTML = helloInstructors.join("");
 
-// document.getElementById("click-me").addEventListener("click", ev => {
-//     // manipulating the browser
-//     window.alert("About to open a new window");
-//     window.open("https://www.appacademy.io/");
+document.getElementById("click-me").addEventListener("click", ev => {
+    // manipulating the browser
+    // alert("About to open a new window");
+    // open("https://www.appacademy.io/");
 
-//     // manipulating the document
-//     document.getElementById("playground").style.color = "red";
-// });
+    // manipulating the document
+    document.getElementById("playground").style.color = "red";
+});
