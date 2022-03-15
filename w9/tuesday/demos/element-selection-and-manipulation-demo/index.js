2a58c8133438a18cd3cc12f058456e45fad34491
Javier Ortiz <javier.ortiz989@gmail.com>
2022-03-15 09:11:11 -0700

    demo

--- w9/tuesday/demos/element-selection-and-manipulation-demo/index.js
+++ w9/tuesday/demos/element-selection-and-manipulation-demo/index.js
@@ -6,22 +6,36 @@ window.onload = () => {
         
         // const instructor = document.querySelector(".instructor");
         // console.log(instructor);
-        // instructor.style.color = "red";
+        // instructor.style.backgroundColor = "red";
 
         const instructors = document.querySelectorAll(".instructor");
-        console.log(instructors);
+        console.log(Array.from(instructors));
         instructors.forEach(instructor => {
             instructor.style.color = "red";
         });
 
+        const url = 'https://p4.wallpaperbetter.com/wallpaper/70/625/78/animal-baby-cute-dog-wallpaper-preview.jpg'
         const playground = document.getElementById("playground");
-        playground.innerHTML = `
+        // playground.innerHTML = `
+        //     <h2 class='red'>Hope you enjoyed the demo!</h2>
+        //     <img 
+        //         src=${url}
+        //     >
+        // `;
+
+
+        // const text = document.createTextNode('Hello world');
+        const text = "hello";
+        div.appendChild(text);
+        const div = document.createElement('div');
+        div.innerHTML = `
             <h2 class='red'>Hope you enjoyed the demo!</h2>
-            <img 
-                src='https://p4.wallpaperbetter.com/wallpaper/70/625/78/animal-baby-cute-dog-wallpaper-preview.jpg'
+            <img
+                class="red-ball"
+                src=${url}
             >
         `;
-
+        playground.children
         // const h2 = document.createElement('h2');
         // const title = document.createTextNode('Hope you enjoyed the demo!')
         // h2.appendChild(title);
